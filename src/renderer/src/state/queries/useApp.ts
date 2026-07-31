fd1e27f0f8fb19d0f152468f1f96bb2dc8c77ff2
Elias-Brendon <102776387@students.swinburne.edu.my>
2026-07-31 14:51:53 +0800

    feat: add useAppVersion renderer hook

---
+++ src/renderer/src/state/queries/useApp.ts
@@ -0,0 +1,9 @@
+import { useQuery, type UseQueryResult } from '@tanstack/react-query'
+
+export function useAppVersion(): UseQueryResult<string> {
+  return useQuery({
+    queryKey: ['app', 'version'],
+    queryFn: () => window.api.app.getVersion(),
+    staleTime: Infinity
+  })
+}
